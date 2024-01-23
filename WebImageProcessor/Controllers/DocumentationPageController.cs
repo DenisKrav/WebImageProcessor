c8d1658ad11e5f9cca59a7a98f1baba2ec1400da
DenisKrav <den.325.2002.5@gmail.com>
2024-01-23 22:51:35 +0200

    Add documentation page

---
+++ WebImageProcessor/Controllers/DocumentationPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebImageProcessor.Controllers
+{
+    public class DocumentationPageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
